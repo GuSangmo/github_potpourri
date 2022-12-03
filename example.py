85493f3b320751f1a0359dcd243391ac9a284c40
gusangmo <gusangmo0605@gmail.com>
2022-12-03 21:57:25 +0900

    feat : add black to pre commit list

---
+++ example.py
@@ -0,0 +1,14 @@
+THRESHOLD = 100
+
+
+class wrong_camel_case:
+    def __init__(self):
+        pass
+
+    # Should be one line
+
+    def wrong_indent(self):
+        pass
+
+
+print("unnecssary long files")
